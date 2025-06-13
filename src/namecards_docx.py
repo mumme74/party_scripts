8a011cbd95f8381f103bc9d000d611c7dc7f43de
Fredrik Johansson <fredrik@mumme.se>
2025-06-13 17:40:28 +0200

    Change same formating to simplify cutting them. Also some graphical tweaks.

--- src/namecards_docx.py
+++ src/namecards_docx.py
@@ -30,16 +30,20 @@ def create_namecard_docx():
 
     #place images in a table for formating 4 on each page
     tbl = doc.add_table(1, 2)
+    tbl.columns[0].width = Cm(4.0)
+    tbl.columns[1].width = Cm(4.0)
     cells = tbl.rows[0].cells
     for i, png in enumerate(sorted(all_png_files(), key=lambda f: int(f.stem))):
         # format the cell and its paragraph
         cell = cells[i % 2]
         cell.vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+        cell.height = Cm(4.0)
+        cell.width = Cm(4.0)
         p = cell.paragraphs[0]
         p.alignment = WD_ALIGN_PARAGRAPH.CENTER
         format = p.paragraph_format
-        format.space_before = Cm(1.0)
-        format.space_after = Cm(0)
+        format.space_before = Cm(0.0)
+        format.space_after = Cm(0.3)
         # add new image
         pic = p.add_run().add_picture(str(prj_dir / 'outdata' / png), width=Pt(300))
         if (i+1) % 2 == 0: # add new row each 2nd cell
